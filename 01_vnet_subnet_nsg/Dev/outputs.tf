8fd8667058a2a8e7bda831cba78d4a626141c035
Cuan Leo <cuanleo@gmail.com>
2023-01-22 13:13:00 +0000

    changes

--- 01_vnet_subnet_nsg/Dev/outputs.tf
+++ 01_vnet_subnet_nsg/Dev/outputs.tf
@@ -7,8 +7,3 @@ output "vm_snet_id" {
   value       = azurerm_subnet.vm_subnet.id
   description = "Outputs the subnet ID"
 }
-
-output "vm_snet_prefixes" {
-  value       = azurerm_subnet.vm_subnet.address_prefixes
-  description = "Outputs the subnet address prefixes"
-}
