1ac65153bad2e79769451d315de7e6d295ca5803
Cuan Leo <cuanleo@gmail.com>
2023-01-22 13:03:05 +0000

    change

--- 01_vnet_subnet_nsg/Dev/main.tf
+++ 01_vnet_subnet_nsg/Dev/main.tf
@@ -35,6 +35,7 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = var.vnet_address_space
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
+  tags                = merge(local.common_tags)
 }
 
 
