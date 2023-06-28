b2daa8493622a168b9c0c4da13fde185f5a869e9
Cuan Leo <cuanleo@gmail.com>
2023-06-28 19:37:17 +0100

    change repo hierarchy

--- production/storage_account/veeam_backup_sa.tf
+++ production/storage_account/veeam_backup_sa.tf
@@ -1,33 +1,11 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "3.62.1"
-    }
-  }
-  backend "azurerm" {}
-}
-
-provider "azurerm" {
-  features {}
-}
-
-terraform {
-  required_version = ">= 1.0"
-}
-
-locals {
-  common_tags = {
-    environment = var.tag_environment
-    createdby   = "Terraform"
-    createdon   = formatdate("DD-MM-YYYY hh:mm ZZZ", timestamp())
-  }
-}
-
 resource "azurerm_resource_group" "rg" {
   name     = var.rg_name
   location = var.rg_location
   tags     = merge(local.common_tags)
+
+  lifecycle {
+    ignore_changes = tags[createdon]
+  }
 }
 
 resource "azurerm_management_lock" "veeam_rg_lock" {
@@ -47,6 +25,10 @@ resource "azurerm_storage_account" "veeam_backup" {
   account_replication_type = var.account_replication_type
   min_tls_version          = "TLS1_2"
   tags                     = merge(local.common_tags)
+
+  lifecycle {
+    ignore_changes = tags[createdon]
+  }
 }
 
 resource "azurerm_storage_account_network_rules" "veeam_backup_sa_nr" {
