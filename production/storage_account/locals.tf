633a05ce0e91042fc2ebeb73671b9129064b419f
Cuan Leo <cuanleo@gmail.com>
2023-07-02 07:20:46 +0100

    change tag value for createdon

--- production/storage_account/locals.tf
+++ production/storage_account/locals.tf
@@ -2,7 +2,7 @@ locals {
   common_tags = {
     environment = var.tag_environment
     createdby   = "Terraform"
-    createdon   = formatdate("DD-MM-YYYY hh:mm ZZZ", timestamp())
+    createdon   = formatdate("DD-MM-YYYY hh:mm ZZZ", time_static.deployment.rfc3339)
     modifiedon  = formatdate("DD-MM-YYYY hh:mm ZZZ", timestamp())
   }
 }
