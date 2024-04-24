53f249bfb32211d0da299cd15ccf37a273c12ac2
Cuan Leo <cuanleo@gmail.com>
2024-04-24 08:47:09 +0200

    fix: remove Azure Pipelines and add azurerm provider information

--- production/storage_account/providers.tf
+++ production/storage_account/providers.tf
@@ -5,7 +5,13 @@ terraform {
       version = "3.100.0"
     }
   }
-  backend "azurerm" {}
+  backend "azurerm" {
+    resource_group_name   = "#{AZURERM_RESOURCE_GROUP_NAME}#"
+    storage_account_name  = "#{AZURERM_STORAGE_ACCOUNT_NAME}#"
+    container_name        = "#{AZURERM_CONTAINER_NAME}#"
+    key                   = "prod.storage.terraform.tfstate"
+    use_oidc              = true
+  }
 }
 
 provider "azurerm" {
