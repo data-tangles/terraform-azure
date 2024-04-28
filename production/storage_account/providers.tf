7badd6012e757e884f7f21ac1c8fc114e29160e3
Cuan Leo <cuanleo@gmail.com>
2024-04-28 14:52:39 +0200

    fix: add additional `azurerm` provider details

--- production/storage_account/providers.tf
+++ production/storage_account/providers.tf
@@ -9,6 +9,9 @@ terraform {
     resource_group_name   = "#{AZURERM_RESOURCE_GROUP_NAME}#"
     storage_account_name  = "#{AZURERM_STORAGE_ACCOUNT_NAME}#"
     container_name        = "#{AZURERM_CONTAINER_NAME}#"
+    tenant_id             = "#{AZURE_TENANT_ID}#"
+    subscription_id       = "#{AZURE_SUBSCRIPTION_ID}#"
+    client_id             = "#{AZURE_CLIENT_ID}#"
     key                   = "prod.storage.terraform.tfstate"
     use_oidc              = true
   }
