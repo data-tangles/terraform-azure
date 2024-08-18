5d1eaee6b368dd6fb8b26e6d6e60a508fb279f03
Cuan Leo <cuanleo@gmail.com>
2024-08-18 11:11:29 +0200

    feat: modify web app to container based

--- modules/azure_app_service/main.tf
+++ modules/azure_app_service/main.tf
@@ -24,5 +24,11 @@ resource "azurerm_linux_web_app" "web_app" {
     ip_restriction {
       ip_address = var.allowed_ip_address
     }
+    application_stack {
+      docker_image_name = var.docker_image_name
+      docker_registry_url = var.docker_registry_url
+      docker_registry_username = var.docker_registry_username
+      docker_registry_password = var.docker_registry_password
+    }
   }
 }
