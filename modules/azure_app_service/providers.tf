723a11914a7879ef2aad1907bbdadc0cd7290f75
Cuan Leo <cuanleo@gmail.com>
2024-08-18 10:21:06 +0200

    fix: add provider configuration

--- modules/azure_app_service/providers.tf
+++ modules/azure_app_service/providers.tf
@@ -5,7 +5,16 @@ terraform {
       version = "3.116.0"
     }
   }
-  backend "azurerm" {}
+  backend "azurerm" {
+    resource_group_name   = "#{AZURERM_RESOURCE_GROUP_NAME}#"
+    storage_account_name  = "#{AZURERM_STORAGE_ACCOUNT_NAME}#"
+    container_name        = "dev-tfstate"
+    tenant_id             = "#{AZURE_TENANT_ID}#"
+    subscription_id       = "#{AZURE_SUBSCRIPTION_ID}#"
+    client_id             = "#{AZURE_CLIENT_ID}#"
+    key                   = "dev.appservice.terraform.tfstate"
+    use_oidc              = true
+  }
 }
 
 provider "azurerm" {
